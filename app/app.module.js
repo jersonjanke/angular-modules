db22b000dbd6b2a61ce3223e6ea48cc797a176ed
jersonjanke <jersonjanke@gmail.com>
2017-04-19 23:09:51 -0300

    Defined  modules names

--- app/app.module.js
+++ app/app.module.js
@@ -1,9 +1,9 @@
 import angular from 'angular';
 import sass from './assets/main.scss'
 import commons from './commons/module';
-import hello from './modules/config/module';
+import modules from './modules/config/module';
 
 angular.module('app', [  
   commons.name,  
-  'modules'
+  modules.name
 ]);
--- app/modules/config/module.js
+++ app/modules/config/module.js
@@ -5,10 +5,16 @@ import serviceHello from '../hello/hello.service';
 import componentWorld from '../world/world.component';
 import serviceWorld from '../world/world.service';
 
+var exports = {
+    name: 'modules'
+}
+
 angular
-  .module('modules', [])
+  .module(exports.name, [])
   .component('hello', componentHello)
   .component('world', componentWorld)
   .service('helloService', serviceHello)    
   .service('worldService', serviceWorld)  
   .config(routing);
+
+export default exports;
